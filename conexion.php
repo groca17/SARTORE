f81cc87d840856108c979553503e488a0b3b046d
groca17 <gabrielroca1702@gmail.com>
2024-12-05 20:47:08 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,14 +1,24 @@
 <?php
+// Nombre del servidor donde se aloja la base de datos
 $servername = "localhost";
+
+// Nombre de usuario para acceder a la base de datos
 $username = "admin";
+
+// Contraseña correspondiente al usuario
 $password = "12345678";
+
+// Nombre de la base de datos a la que se conectará
 $dbname = "sartore";
 
-// Crear conexión
+// Crear conexión a la base de datos utilizando la clase mysqli
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexión
+// Verificar si la conexión fue exitosa
 if ($conn->connect_error) {
+    // Si hay un error en la conexión, se muestra un mensaje y se detiene la ejecución del script
     die("Conexión fallida: " . $conn->connect_error);
 }
+
+// Si no hay errores, la conexión está lista para ser utilizada
 ?>
